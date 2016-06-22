35318a4ed1cdeaa78a0c13284cdd3872b02c2bbb
K. Z. Watkins <kzw@acm.org>
2016-06-22 00:56:09 -0400

    Meetups finally showing. Pretty printing needed next.

--- src/scripts/meetups.js
+++ src/scripts/meetups.js
@@ -1,26 +1,35 @@
 var url ="https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=Code-For-Dayton&page=20";
+var data;
 
-
-$(document).ready(function () {
-  $('#get-data').click(function () {
-    var showData = $('#show-data');
-
-    $.getJSON(url, function (data) {
-      console.log(data);
-
-      var items = data.items.map(function (item) {
-       return item.key + ': ' + item.value;
-      });
-
-   showData.empty();
-
-      if (items.length) {
-        var content = '<li>' + items.join('</li><li>') + '</li>';
-        var list = $('<ul />').html(content);
-        showData.append(list);
-      }
-    });
-
-    showData.text('Loading the JSON file.');
-  });
+$.ajax({
+  type: 'GET',
+  url: url,
+  data: data,
+  async: false,
+  beforeSend: function (xhr) {
+    if (xhr && xhr.overrideMimeType) {
+      xhr.overrideMimeType('application/json;charset=utf-8');
+    }
+  },
+  dataType: 'jsonp',
+  success: function (data) {
+  	var results = data.results;
+    console.log(results);
+    
+            var output="<dl>";
+ 
+             for (var i in results) {
+             	var date = new Date(results[i].time);
+             	
+                output+="<dd>" + results[i].name + ",  " + date.toString("MMM dd yy") + ",  " + getPlace(results[i].venue) + ", <a href= '" + results[i].event_url + "'> Join the Meetup! </a>, " + results[i].description + ", " + results[i].yes_rsvp_count + " Volunteers so far!</dd>";
+            }
+            output+="</dl>";
+            
+            document.getElementById("show-data").innerHTML = output;
+  }
 });
+
+var getPlace = function(venue){
+	var result = venue.name + ", " + venue.address_1 + ", " + venue.city + ", " + venue.state;
+	return result;
+}
